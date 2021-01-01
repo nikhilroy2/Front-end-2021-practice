b8467f9b6314f9d734fa671e38756676e065821c
FENR1 USER <fenr1user@biadsconsulting.org>
2021-01-01 14:32:55 +0600

    menu clickable scrolling added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ function trxClick(val){
 function trxReset(val){
     $('.control_output span').eq(0).text(val);
 }
+function navLink(val){
+    $('[data-nav-link]').removeClass('active')
+    let has_class = $(val).hasClass('active');
+    if(!has_class){
+        $(val).addClass('active')
+    }
+}
